19acf950157442e26aed706af831f69677fca446
tecarramone <tecarramone@gmail.com>
2020-04-24 13:23:42 -0500

    added js and store

--- index.js
+++ index.js
@@ -1,139 +1,143 @@
-var myQuestions = [
-	{
-		question: "What war did Captain America fight in?",
-		answers: {
-			a: 'World War 1',
-			b: 'World War 2',
-            c: 'The Korean War',
-            d: 'The Vietnam War'
-		},
-		correctAnswer: 'b'
-	},
-	{
-		question: "What was Iron Man’s Nickname for Thor?",
-		answers: {
-			a: 'Ice Man',
-			b: 'Point Break',
-            c: 'Popsicle',
-            d: 'Strongest Avenger'
-		},
-		correctAnswer: 'd'
-    }
-    {
-        question: "Where is the Avengers Tower Located?",
-		answers: {
-			a: 'Chicago',
-			b: 'New York',
-            c: 'California',
-            d: 'Texas'
-        },
-        correctAnswer: 'b'
-    }
-    {
-        question: "What is Captain Americas shield made of?",
-		answers: {
-			a: 'Mithral',
-			b: 'Adamacium',
-            c: 'Titanium',
-            d: 'Vibranium'
-        },
-        correctAnswer: 'd'
-    }
-    {
-        question: "What is Thor's hammer called??",
-		answers: {
-			a: 'Stormbreaker',
-			b: 'Mjolnir',
-            c: 'Stormbringer',
-            d: 'Doomhammer'
-        },
-        correctAnswer: 'a'
-    }
-];
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-    function showQuestions(questions, quizContainer){
-        // we'll need a place to store the output and the answer choices
-        var output = [];
-        var answers;
-
-        // for each question...
-        for(var i=0; i<questions.length; i++){
-            
-            // first reset the list of answers
-            answers = [];
-
-            // for each available answer...
-            for(letter in questions[i].answers){
-
-                // ...add an html radio button
-                answers.push(
-                    '<label>'
-                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-                        + letter + ': '
-                        + questions[i].answers[letter]
-                    + '</label>'
-                );
-            }
-
-            // add this question and its answers to the output
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '</div>'
-            );
-        }
-
-        // finally combine our output list into one string of html and put it on the page
-        quizContainer.innerHTML = output.join('');
-    }
-
-
-    function showResults(questions, quizContainer, resultsContainer){
-        
-        // gather answer containers from our quiz
-        var answerContainers = quizContainer.querySelectorAll('.answers');
-        
-        // keep track of user's answers
-        var userAnswer = '';
-        var numCorrect = 0;
-        
-        // for each question...
-        for(var i=0; i<questions.length; i++){
-
-            // find selected answer
-            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-            
-            // if answer is correct
-            if(userAnswer===questions[i].correctAnswer){
-                // add to the number of correct answers
-                numCorrect++;
-                
-                // color the answers green
-                answerContainers[i].style.color = 'lightgreen';
-            }
-            // if answer is wrong or blank
-            else{
-                // color the answers red
-                answerContainers[i].style.color = 'red';
-            }
-        }
-
-        // show number of correct answers out of total
-        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
-
-    // show questions right away
-    showQuestions(questions, quizContainer);
-    
-    // on submit, show results
-    submitButton.onclick = function(){
-        showResults(questions, quizContainer, resultsContainer);
-    }
-
+function startQuiz() {
+    STORE.questionIndex = 0;
+    STORE.score = 0;
+    $('#begin-quiz button').click(function() {
+        const parentID = $(this).closest('section').attr('id');
+        console.log(parentID);
+        renderQuestions(parentID);
+    });
 }
+
+function evalQuestion() {
+    $('body').on("click", 'form #js-submit-button', function(event) {
+            event.preventDefault();
+
+            const parentID = $(this).closest('section').attr('id');
+            const selAnswer = $(this).closest('section').find('.checked').attr('value');
+
+            if (!selAnswer) {
+                return promptSelection('No selection!', style.noAnswer);
+            } else if(checkAnswer(selAnswer) == 'correct') {
+                // Give user feedback
+                STORE.questionIndex += 1;
+                STORE.score += 1;
+                promptSelection('Correct!', style.correct);
+                nextButton();
+            } else if(checkAnswer(selAnswer) == 'inCorrect') {
+                // Give user feedback
+                const feedback = 'incorrect! correct answer is ' +  STORE.questions[STORE.questionIndex].correctAnswer;
+                STORE.questionIndex += 1;
+                promptSelection(feedback, style.incorrect);
+                nextButton();
+            }
+    })
+}
+
+function selectedOption() {
+    $('body').on('click','.js-form-option', event => {
+        console.log('Hi');
+        const targetAnswer = $(event.currentTarget);
+
+        const otherAnswers = $('.js-form-option').not(targetAnswer);
+
+        const pressedBool = $(targetAnswer).attr('aria-pressed') === 'true';
+
+        otherAnswers.removeClass('checked').attr('aria-pressed', false);
+
+        targetAnswer.addClass('checked').attr('aria-pressed', !pressedBool);
+    })
+}
+
+function whenNextButtonClicked() {
+    $('body').on("click",'form #js-next-button', function(event) {
+        event.preventDefault();
+        const parentID = $(this).closest('section').attr('id');
+        console.log(parentID);
+        renderQuestions(parentID);
+    })
+}
+
+function tryAgain() {
+    $('body').on("click", '#results-quiz button', function(event) {
+        const parentID = $(this).closest('section').attr('id');
+        STORE.questionIndex = 0;
+        STORE.score = 0;
+        renderQuestions(parentID);
+    });
+}
+
+function promptSelection(displayText, cl) {
+    console.log(cl);
+    const feedback = $('form').closest('form').find('#feedback');
+    console.log(feedback);
+    if(!feedback) {
+        $('form').after('<p id="feedback" class="'+ cl +'">' + displayText + '</p>');
+    } else {
+        $('form').find('#feedback').replaceWith('<p id="feedback" class="'+ cl +'">' + displayText + '</p>');
+    }
+}
+
+function checkAnswer(answer) {
+    if(answer == STORE.questions[STORE.questionIndex].correctAnswer) {
+        return 'correct';
+    } else if(answer != STORE.questions[STORE.questionIndex].correctAnswer) {
+        return 'inCorrect';
+    } else {
+        return 'noAnswer'
+    }
+}
+
+function nextButton() {
+    $('form #js-submit-button').replaceWith("<input type='submit' id='js-next-button' class='main-button' value='Next'></input>");
+}
+
+function renderQuestion(index, parentId) {
+    const questionIndex = STORE.questionIndex + 1;
+    $('#' + parentId).replaceWith(
+        "<section id='question-quiz'>" +
+            "<p id='score'>"+ "SCORE:" + STORE.score + "</p>" +
+            "<p id='question-num'>" + "QUESTION:" + (questionIndex) +"</p>" +
+            "<h2>" + STORE.questions[index].question + "</h2>" +
+            "<form onsubmit='return false;' class='js-quiz-form'>" +
+                generateQuizOptionsString(index) +
+                "<input type='submit' id='js-submit-button' class='main-button' value='Submit'>" +
+                "<p id='feedback'></p>" +
+            "</form>" +
+        "</section>"
+    );
+}
+
+function generateQuizOptionsString(index) {
+    const items = STORE.questions[index].choices.map((choice, idx) => "<input type='button' aria-pressed='false' class='form-option js-form-option' name='answer' value='" + STORE.questions[index].choices[idx] + "'>");
+    return items.join("");
+}
+
+function renderResults(parentID) {
+    $('#' + parentID).replaceWith(
+        '<section id="results-quiz">' +
+            '<h2>Quiz Results</h2>' +
+            '<p id="results">Results:'+ STORE.score + '/' + STORE.questions.length + '</p>' +
+            '<button class="main-button" type="button">Try Again</button>' +
+        '</section>'
+    )
+}
+
+function renderQuestions(parentID) {
+    const index = STORE.questionIndex;
+    if(STORE.questionIndex == STORE.questions.length) {
+        renderResults(parentID);
+    } else {
+        renderQuestion(index, parentID);
+    }
+}
+
+
+function handleQuizApp() {
+    startQuiz();
+    selectedOption();
+    evalQuestion();
+    tryAgain();
+    whenNextButtonClicked();
+}
+
+$(handleQuizApp)
