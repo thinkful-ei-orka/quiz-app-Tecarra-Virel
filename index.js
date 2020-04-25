ed1ca6ea19f9c08bb61f218b117db135ef1ea3e9
Virel Montes <jovanymontesv@gmail.com>
2020-04-25 14:23:53 -0700

    added button css

--- index.js
+++ index.js
@@ -1,100 +1,100 @@
 function startQuiz() {
-    STORE.questionIndex = 0;
-    STORE.score = 0;
-    $('#begin-quiz button').click(function() {
-        const parentID = $(this).closest('section').attr('id');
-        console.log(parentID);
-        renderQuestions(parentID);
-    });
+  STORE.questionIndex = 0;
+  STORE.score = 0;
+  $('#begin-quiz button').click(function() {
+    const parentID = $(this).closest('section').attr('id');
+    console.log(parentID);
+    renderQuestions(parentID);
+  });
 }
 
 function evalQuestion() {
-    $('body').on("click", 'form #js-submit-button', function(event) {
-            event.preventDefault();
+  $('body').on("click", 'form #js-submit-button', function(event) {
+    event.preventDefault();
 
-            const parentID = $(this).closest('section').attr('id');
-            const selAnswer = $(this).closest('section').find('.checked').attr('value');
+    const parentID = $(this).closest('section').attr('id');
+    const selAnswer = $(this).closest('section').find('.checked').attr('value');
 
-            if (!selAnswer) {
-                return promptSelection('No selection!', style.noAnswer);
-            } else if(checkAnswer(selAnswer) == 'correct') {
-                // Give user feedback
-                STORE.questionIndex += 1;
-                STORE.score += 1;
-                promptSelection('Correct!', style.correct);
-                nextButton();
-            } else if(checkAnswer(selAnswer) == 'inCorrect') {
-                // Give user feedback
-                const feedback = 'incorrect! correct answer is ' +  STORE.questions[STORE.questionIndex].correctAnswer;
-                STORE.questionIndex += 1;
-                promptSelection(feedback, style.incorrect);
-                nextButton();
-            }
-    })
+    if (!selAnswer) {
+      return promptSelection('No selection!', style.noAnswer);
+    } else if(checkAnswer(selAnswer) == 'correct') {
+      // Give user feedback
+      STORE.questionIndex += 1;
+      STORE.score += 1;
+      promptSelection('Correct!', style.correct);
+      nextButton();
+    } else if(checkAnswer(selAnswer) == 'inCorrect') {
+      // Give user feedback
+      const feedback = 'incorrect! correct answer is ' +  STORE.questions[STORE.questionIndex].correctAnswer;
+      STORE.questionIndex += 1;
+      promptSelection(feedback, style.incorrect);
+      nextButton();
+    }
+  })
 }
 
 function selectedOption() {
-    $('body').on('click','.js-form-option', event => {
-        console.log('Hi');
-        const targetAnswer = $(event.currentTarget);
+  $('body').on('click','.js-form-option', event => {
+    console.log('Hi');
+    const targetAnswer = $(event.currentTarget);
 
-        const otherAnswers = $('.js-form-option').not(targetAnswer);
+    const otherAnswers = $('.js-form-option').not(targetAnswer);
 
-        const pressedBool = $(targetAnswer).attr('aria-pressed') === 'true';
+    const pressedBool = $(targetAnswer).attr('aria-pressed') === 'true';
 
-        otherAnswers.removeClass('checked').attr('aria-pressed', false);
+    otherAnswers.removeClass('checked').attr('aria-pressed', false);
 
-        targetAnswer.addClass('checked').attr('aria-pressed', !pressedBool);
-    })
+    targetAnswer.addClass('checked').attr('aria-pressed', !pressedBool);
+  })
 }
 
 function whenNextButtonClicked() {
-    $('body').on("click",'form #js-next-button', function(event) {
-        event.preventDefault();
-        const parentID = $(this).closest('section').attr('id');
-        console.log(parentID);
-        renderQuestions(parentID);
-    })
+  $('body').on("click",'form #js-next-button', function(event) {
+    event.preventDefault();
+    const parentID = $(this).closest('section').attr('id');
+    console.log(parentID);
+    renderQuestions(parentID);
+  })
 }
 
 function tryAgain() {
-    $('body').on("click", '#results-quiz button', function(event) {
-        const parentID = $(this).closest('section').attr('id');
-        STORE.questionIndex = 0;
-        STORE.score = 0;
-        renderQuestions(parentID);
-    });
+  $('body').on("click", '#results-quiz button', function(event) {
+    const parentID = $(this).closest('section').attr('id');
+    STORE.questionIndex = 0;
+    STORE.score = 0;
+    renderQuestions(parentID);
+  });
 }
 
 function promptSelection(displayText, cl) {
-    console.log(cl);
-    const feedback = $('form').closest('form').find('#feedback');
-    console.log(feedback);
-    if(!feedback) {
-        $('form').after('<p id="feedback" class="'+ cl +'">' + displayText + '</p>');
-    } else {
-        $('form').find('#feedback').replaceWith('<p id="feedback" class="'+ cl +'">' + displayText + '</p>');
-    }
+  console.log(cl);
+  const feedback = $('form').closest('form').find('#feedback');
+  console.log(feedback);
+  if(!feedback) {
+    $('form').after('<p id="feedback" class="'+ cl +'">' + displayText + '</p>');
+  } else {
+    $('form').find('#feedback').replaceWith('<p id="feedback" class="'+ cl +'">' + displayText + '</p>');
+  }
 }
 
 function checkAnswer(answer) {
-    if(answer == STORE.questions[STORE.questionIndex].correctAnswer) {
-        return 'correct';
-    } else if(answer != STORE.questions[STORE.questionIndex].correctAnswer) {
-        return 'inCorrect';
-    } else {
-        return 'noAnswer'
-    }
+  if(answer == STORE.questions[STORE.questionIndex].correctAnswer) {
+    return 'correct';
+  } else if(answer != STORE.questions[STORE.questionIndex].correctAnswer) {
+    return 'inCorrect';
+  } else {
+    return 'noAnswer'
+  }
 }
 
 function nextButton() {
-    $('form #js-submit-button').replaceWith("<input type='submit' id='js-next-button' class='main-button' value='Next'></input>");
+  $('form #js-submit-button').replaceWith("<input type='submit' id='js-next-button' class='main-button' value='Next'></input>");
 }
 
 function renderQuestion(index, parentId) {
-    const questionIndex = STORE.questionIndex + 1;
-    $('#' + parentId).replaceWith(
-        "<section id='question-quiz'>" +
+  const questionIndex = STORE.questionIndex + 1;
+  $('#' + parentId).replaceWith(
+    "<section id='question-quiz'>" +
             "<p id='score'>"+ "SCORE:" + STORE.score + "</p>" +
             "<p id='question-num'>" + "QUESTION:" + (questionIndex) +"</p>" +
             "<h2>" + STORE.questions[index].question + "</h2>" +
@@ -104,40 +104,40 @@ function renderQuestion(index, parentId) {
                 "<p id='feedback'></p>" +
             "</form>" +
         "</section>"
-    );
+  );
 }
 
 function generateQuizOptionsString(index) {
-    const items = STORE.questions[index].choices.map((choice, idx) => "<input type='button' aria-pressed='false' class='form-option js-form-option' name='answer' value='" + STORE.questions[index].choices[idx] + "'>");
-    return items.join("");
+  const items = STORE.questions[index].choices.map((choice, idx) => "<button type='button' aria-pressed='false' class='form-option js-form-option' name='answer' value='" + STORE.questions[index].choices[idx] + "'> " + STORE.questions[index].choices[idx] + "</button>");
+  return items.join("");
 }
 
 function renderResults(parentID) {
-    $('#' + parentID).replaceWith(
-        '<section id="results-quiz">' +
+  $('#' + parentID).replaceWith(
+    '<section id="results-quiz">' +
             '<h2>Quiz Results</h2>' +
             '<p id="results">Results:'+ STORE.score + '/' + STORE.questions.length + '</p>' +
             '<button class="main-button" type="button">Try Again</button>' +
         '</section>'
-    )
+  )
 }
 
 function renderQuestions(parentID) {
-    const index = STORE.questionIndex;
-    if(STORE.questionIndex == STORE.questions.length) {
-        renderResults(parentID);
-    } else {
-        renderQuestion(index, parentID);
-    }
+  const index = STORE.questionIndex;
+  if(STORE.questionIndex == STORE.questions.length) {
+    renderResults(parentID);
+  } else {
+    renderQuestion(index, parentID);
+  }
 }
 
 
 function handleQuizApp() {
-    startQuiz();
-    selectedOption();
-    evalQuestion();
-    tryAgain();
-    whenNextButtonClicked();
+  startQuiz();
+  selectedOption();
+  evalQuestion();
+  tryAgain();
+  whenNextButtonClicked();
 }
 
 $(handleQuizApp)
--- store.js
+++ store.js
@@ -1,62 +1,62 @@
 const STORE = {
-    questions: [
-        {
-            question: 'What war did Captain America fight in?',
-            choices: [
-                'World War 1',
-                'World War 2',
-                'The Vietnam War',
-                'The Korean War',
-            ],
-            correctAnswer: 'World War 1'
-        },
-        {
-            question: 'What was Iron Man’s Nickname for Thor?',
-            choices: [
-                'Ice Man',
-                'Point Break',
-                'Popsicle',
-                'Strongest Avenger',
-            ],
-            correctAnswer: 'Point Break'
-        },
-        {
-            question: 'Where is Avengers Tower Located?',
-            choices: [
-                'Chicagp',
-                'Cleveland',
-                'New York',
-                'Texas',
-            ],
-            correctAnswer: 'New York'
-        },
-        {
-            question: 'What is Captain Americas shield made of?',
-            choices: [
-                'Mithral',
-                'Adamacium',
-                'Titanium',
-                'Vibranium',
-            ],
-            correctAnswer: 'Vibranium'
-        }, 
-        {
-            question: "What is Thor's hammer called",
-            choices: [
-                'Stormbreaker',
-                'Mjolnir',
-                'Stormbringer',
-                'Doomhammer',
-            ],
-            correctAnswer: 'Stormbreaker'
-        }, 
-    ],
-    score: 0,
-    questionIndex: 0,
+  questions: [
+    {
+      question: 'What war did Captain America fight in?',
+      choices: [
+        'World War 1',
+        'World War 2',
+        'The Vietnam War',
+        'The Korean War',
+      ],
+      correctAnswer: 'World War 2'
+    },
+    {
+      question: 'What was Iron Man’s Nickname for Thor?',
+      choices: [
+        'Ice Man',
+        'Point Break',
+        'Popsicle',
+        'Legolas',
+      ],
+      correctAnswer: 'Point Break'
+    },
+    {
+      question: 'Where is Avengers Tower Located?',
+      choices: [
+        'Chicagp',
+        'Cleveland',
+        'New York',
+        'Texas',
+      ],
+      correctAnswer: 'New York'
+    },
+    {
+      question: 'What is Captain Americas shield made of?',
+      choices: [
+        'Mithral',
+        'Adamacium',
+        'Titanium',
+        'Vibranium',
+      ],
+      correctAnswer: 'Vibranium'
+    }, 
+    {
+      question: "What is Thor's new hammer called",
+      choices: [
+        'Stormbreaker',
+        'Mjolnir',
+        'Stormbringer',
+        'Doomhammer',
+      ],
+      correctAnswer: 'Stormbreaker'
+    }, 
+  ],
+  score: 0,
+  questionIndex: 0,
 }
 
 const style = {
-    'correct': 'feedback-correct',
-    'incorrect': 'feedback-incorrect',
-    'noAnswer': 'feedback-noAnswer',
+  'correct': 'feedback-correct',
+  'incorrect': 'feedback-incorrect',
+  'noAnswer': 'feedback-noAnswer',
 }
